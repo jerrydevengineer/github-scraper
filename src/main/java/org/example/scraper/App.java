904b3e4ea625c376f2d6920b13923e99c76d7c6b
jerry8534 <jerryjjinu@gmail.com>
2025-09-24 21:06:29 +0900

    Fix: API url change

--- src/main/java/org/example/scraper/App.java
+++ src/main/java/org/example/scraper/App.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 
 public class App {
     // The URL for the API
-    private static final String API_URL = "https://api.gitterapp.com/repositories?language=java&since=daily";
+//    private static final String API_URL = "https://api.gitterapp.com/repositories?language=java&since=daily";
 //    private static final String API_BASE_URL = "https://api.g-h-t.de/repositories";
 
     public static void main(String[] args) throws IOException {
@@ -35,8 +35,8 @@ public class App {
         System.out.println("Fetching trending repositories for language: " + language);
 
         // 2. Build the API URL dynamically
-//        String apiUrl = "https://api.gitterapp.com/repositories?language=" + language + "&since=" + timeframe;
-        String apiUrl = API_URL + "?language=" + language + "&since=" + timeframe;
+        String apiUrl = "https://api.gitterapp.com/repositories?language=" + language + "&since=" + timeframe;
+//        String apiUrl = API_URL + "?language=" + language + "&since=" + timeframe;
 
         // 3. Fetch the data from the API
         OkHttpClient client = new OkHttpClient();
